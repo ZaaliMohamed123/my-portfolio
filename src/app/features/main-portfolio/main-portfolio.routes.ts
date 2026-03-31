731a2399fd0cc1b75884427922f695ebd89a91f3
Zaali Mohamed <zaali.mohamed3002@gmail.com>
2026-03-31 15:06:17 +0100

    chore: remove unused files and fix header overflow on 1200-1329px screens

--- src/app/features/main-portfolio/main-portfolio.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const mainPortfolioRoutes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./components/home/home').then(c => c.Home)
-  }
-];
